ff78cbddbfc20f0d41f124dbd38b0f91c67f7982
Forhad Khan <fk.nafi11@gmail.com>
2026-01-23 14:07:51 +0600

    popular Flights offer lib created

---
+++ src/lib/popularFlights.ts
@@ -0,0 +1,51 @@
+import { FlightOffer } from "@/types/allTypes";
+
+export const popularFlights: FlightOffer[] = [
+  {
+    id: "popular-1",
+    price: { grandTotal: "320" },
+    itineraries: [
+      {
+        duration: "PT3H15M",
+        segments: [
+          {
+            departure: { at: "2026-02-01T08:00:00" },
+            carrierCode: "DL",
+          },
+        ],
+      },
+    ],
+  } as FlightOffer,
+
+  {
+    id: "popular-2",
+    price: { grandTotal: "360" },
+    itineraries: [
+      {
+        duration: "PT4H10M",
+        segments: [
+          {
+            departure: { at: "2026-02-01T09:30:00" },
+            carrierCode: "UA",
+          },
+        ],
+      },
+    ],
+  } as FlightOffer,
+
+  {
+    id: "popular-3",
+    price: { grandTotal: "410" },
+    itineraries: [
+      {
+        duration: "PT5H00M",
+        segments: [
+          {
+            departure: { at: "2026-02-01T11:00:00" },
+            carrierCode: "AA",
+          },
+        ],
+      },
+    ],
+  } as FlightOffer,
+];
